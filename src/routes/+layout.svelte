00bb8312c8139a51343244efbf94a16f118639f2
sipofnightshade <gregorymcmillan96@gmail.com>
2024-03-20 09:40:16 -0400

    setup-- darkmode and it's toggle button✅

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
 	import '../app.pcss';
+	import { ModeWatcher } from 'mode-watcher';
+	import ModeToggle from '$lib/components/ModeToggle/ModeToggle.svelte';
 </script>
 
+<ModeWatcher />
 <slot />
+
+<!-- <ModeToggle /> -->
