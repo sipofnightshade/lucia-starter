32327ad8bac5f917cd814094085b51f43affc282
sipofnightshade <gregorymcmillan96@gmail.com>
2024-03-20 16:15:06 -0400

    added-- microsoft, google, github, facebook icons ✅

---
+++ src/lib/Icons/Microsoft.svelte
@@ -0,0 +1,14 @@
+<svg
+  class={$$props.class}
+  viewBox="0 0 32 32"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    d="M14.6667 14.6666L4 14.6666L4 3.99996L14.6667 3.99996L14.6667 14.6666Z"
+    fill="#FF5722"
+  />
+  <path d="M28 14.6666L17.3333 14.6666V3.99996L28 3.99996V14.6666Z" fill="#4CAF50" />
+  <path d="M28 28H17.3333V17.3333H28V28Z" fill="#FFC107" />
+  <path d="M14.6667 28H4L4 17.3333H14.6667L14.6667 28Z" fill="#03A9F4" />
+</svg>
---
+++ src/lib/Icons/index.ts
@@ -0,0 +1,4 @@
+export { default as Google } from '$lib/Icons/Google.svelte';
+export { default as Github } from '$lib/Icons/Github.svelte';
+export { default as Microsoft } from '$lib/Icons/Microsoft.svelte';
+export { default as Facebook } from '$lib/Icons/Facebook.svelte';
