4682a7dcf41e8f2fedf1f969de87f2e7ff10afab
sipofnightshade <gregorymcmillan96@gmail.com>
2024-04-03 21:10:08 -0400

    updated-- tailwind breakpoints component

--- src/lib/components/ShowTWBreakpoints.svelte
+++ src/lib/components/ShowTWBreakpoints.svelte
@@ -1,9 +1,10 @@
 <div
-	class="absolute bottom-3 right-3 rounded-2xl bg-accent p-3 font-bold uppercase text-accent-foreground"
+	class="absolute right-3 top-3 rounded-2xl bg-purple-500 p-3 font-bold uppercase text-primary-foreground"
 >
 	<span class="sm:hidden">base</span>
 	<span class="hidden sm:block md:hidden">sm</span>
 	<span class="hidden md:block lg:hidden">md</span>
 	<span class="hidden lg:block xl:hidden">lg</span>
 	<span class="hidden xl:block 2xl:hidden">xl</span>
+	<span class="3xl:hidden hidden 2xl:block">xl</span>
 </div>
