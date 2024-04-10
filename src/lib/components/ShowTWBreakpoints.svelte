baa5a731a66ac90a1c32aa8d853c0e812fa5352a
sipofnightshade <gregorymcmillan96@gmail.com>
2024-04-10 18:08:26 -0400

    updated-- Breakpoints component to show 2xl ✅

--- src/lib/components/ShowTWBreakpoints.svelte
+++ src/lib/components/ShowTWBreakpoints.svelte
@@ -6,5 +6,5 @@
 	<span class="hidden md:block lg:hidden">md</span>
 	<span class="hidden lg:block xl:hidden">lg</span>
 	<span class="hidden xl:block 2xl:hidden">xl</span>
-	<span class="3xl:hidden hidden 2xl:block">xl</span>
+	<span class="3xl:hidden hidden 2xl:block">2xl</span>
 </div>
