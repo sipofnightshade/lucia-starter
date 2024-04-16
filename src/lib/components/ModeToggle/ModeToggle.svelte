c5b850bd7f22ee0cd17f6a9cbf736437440ba002
sipofnightshade <gregorymcmillan96@gmail.com>
2024-04-16 09:50:16 -0400

    updated-- ModalToggle Icons ✅

--- src/lib/components/ModeToggle/ModeToggle.svelte
+++ src/lib/components/ModeToggle/ModeToggle.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-	import Sun from 'svelte-radix/Sun.svelte';
-	import Moon from 'svelte-radix/Moon.svelte';
+	import Sun from 'lucide-svelte/icons/sun';
+	import MoonStar from 'lucide-svelte/icons/moon-star';
+
 	import { Button } from '$lib/components/ui/button/index.js';
 
 	import { toggleMode } from 'mode-watcher';
@@ -10,7 +11,7 @@
 	<Sun
 		class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
 	/>
-	<Moon
+	<MoonStar
 		class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
 	/>
 	<span class="sr-only">Toggle theme</span>
